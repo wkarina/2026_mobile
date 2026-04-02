9893d9162cbf4be086428d4c08d4b2de66dcf77e
KARINA DE ARAUJO FELISBINO <karina.afelisbino@senacsp.edu.br>
2026-04-02 09:04:04 -0300

    Inicializando

---
+++ taskManager/src/test/java/br/senac/sp/pi/taskManager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.sp.pi.taskManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
